a8acddd86ee389c59db7b3836fcf90ca28290afb
tarikjamil <tarikjamill@gmail.com>
2023-11-03 18:28:54 +0100

    edits

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
   var scrollTop = 0;
   $(window).scroll(function () {
     scrollTop = $(window).scrollTop();
-    if (scrollTop >= 100) {
+    if (scrollTop >= 80) {
       $(".navbar").addClass("is--scrolled");
-    } else if (scrollTop < 100) {
+    } else if (scrollTop < 80) {
       $(".navbar").removeClass("is--scrolled");
     }
   });
